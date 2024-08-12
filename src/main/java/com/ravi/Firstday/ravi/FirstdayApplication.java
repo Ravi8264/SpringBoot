4db23697a7291fbc8dcf8cd869902addb0f1be06
Ravi <rskumar.hit@gmail.com>
2024-08-12 12:38:26 +0530

    FirstLac

---
+++ src/main/java/com/ravi/Firstday/ravi/FirstdayApplication.java
@@ -0,0 +1,9 @@
+package com.ravi.Firstday.ravi;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class FirstdayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FirstdayApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ravi/Firstday/ravi/FirstdayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravi.Firstday.ravi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstdayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
